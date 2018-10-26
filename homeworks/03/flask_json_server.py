f08d5e800e6e46b3c1780ed8b5db47e2e780cd49
marat1804 <maratbest6@gmail.com>
2018-10-26 21:39:31 +0300

    knn+flask

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route("/get_classifier_result/<version>", methods=['GET', 'POST'])
 def return_classifier_result(version):
+    if request.method=='POST':
+        if int(version)==1:
+            return jsonify(version=1,predict=request.get_json()['predict'])
+       	return jsonify(version=0,predict=request.get_json()['old_predict'])
     #TODO прочитать из полученного запроса json-контент
     #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
     #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
@@ -11,7 +15,7 @@ def return_classifier_result(version):
 @app.route("/")
 def hello():
     #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    return "Для получения результата /get_classifier_result/<version:0,1> "
 
 if __name__ == "__main__":
     app.run()
